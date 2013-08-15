c3e2e47e3687e0c24aed3e24d6a5704f26306ec3
Chris Mytton <self@hecticjeff.net>
2013-08-15 19:47:15 +0100

    Remove old failedloginblocker

--- failedloginblocker/exceptions.py
+++
@@ -1,5 +0,0 @@
-class LoginBlockedError( Exception ):
-  def __init__( self ):
-    msg = "Your account has been locked due to too many failed login attempts."
-    msg += " Contact us to have your account reactivated."
-    super( LoginBlockedError, self ).__init__( msg )
