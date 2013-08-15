ac7370b54c34b3f2be99a6e956a77ef0784753b0
Chris Mytton <self@hecticjeff.net>
2013-08-15 18:49:33 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='django-failedloginblocker',
+    version='1.0.0',
+    packages=['failedloginblocker'],
+    license=open('LICENSE').read(),
+    long_description=open('README').read(),
+    author='Alex Kuhl',
+    maintainer='mySociety',
+    maintainer_email='programmers@mysociety.org',
+    url='https://github.com/mysociety/django-failedloginblocker',
+)
