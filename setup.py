8790d952c4bbddd4cb99433b17b2804cb93d45ec
Chris Mytton <self@hecticjeff.net>
2013-08-16 11:56:24 +0100

    Explicitly specify packages in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='django-failedloginblocker',
     version='1.0.0',
-    packages=find_packages(),
+    packages=['failedloginblocker'],
     package_data={
         '': ['*.html'],
     },
