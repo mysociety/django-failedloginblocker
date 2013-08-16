35f31ef91ec5bdffd0a9b874da315b5c9c4b23a9
Chris Mytton <self@hecticjeff.net>
2013-08-16 11:38:10 +0100

    Update setup.py metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-failedloginblocker',
     version='1.0.0',
-    packages=['failedloginblocker'],
+    packages=find_packages(),
+    package_data={
+        '': ['*.html'],
+    },
     license=open('LICENSE').read(),
     long_description=open('README').read(),
-    author='Alex Kuhl',
     maintainer='mySociety',
     maintainer_email='programmers@mysociety.org',
     url='https://github.com/mysociety/django-failedloginblocker',
